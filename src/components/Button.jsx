0d461fd023eb9d9776776863c15c74690daec7c5
veronosmani <veron.osmani@gmail.com>
2024-06-21 11:54:34 +0200

    modified:   src/components/Button.jsx 	new file:   src/components/ReviewCard.jsx 	modified:   src/sections/CostumerReviews.jsx 	modified:   src/sections/Subscribe.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,13 +4,16 @@ const Button = ({
   backgroundColor,
   textColor,
   borderColor,
+  fullWidth,
 }) => {
   return (
     <button
       className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none
         ${
           backgroundColor
-          ? `${backgroundColor} ${textColor} ${borderColor}` : "bg-coral-red text-white border-coral-red"} rounded-full w-full}`}
+            ? `${backgroundColor} ${textColor} ${borderColor}`
+            : "bg-coral-red text-white border-coral-red"
+        } rounded-full ${fullWidth && "w-full"}"}`}
     >
       {label}
       {iconURL && (
