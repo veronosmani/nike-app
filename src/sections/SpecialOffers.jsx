f16189c7182a0519ab183854dbffea8168b7e982
veronosmani <veron.osmani@gmail.com>
2024-06-21 10:56:33 +0200

    modified:   src/components/Button.jsx 	modified:   src/sections/SpecialOffers.jsx

--- src/sections/SpecialOffers.jsx
+++ src/sections/SpecialOffers.jsx
@@ -1,5 +1,6 @@
 import { offer } from "../assets/images";
-
+import { arrowRight} from '../assets/icons'
+import Button from "../components/Button";
 const SpecialOffers = () => {
   return (
     <section className="flex justify-wrap items-center max-xl:flex-col-reverse gap-10 max-container">
@@ -10,6 +11,25 @@ const SpecialOffers = () => {
           height={687}
           className="object-contain w-full"
         />
+      </div>
+      <div className="flex flex-1 flex-col">
+        <h2 className="font-palanquin capitalize text-4xl lg:max-w-lg font-bold">
+          <span className="text-coral-red"> Special </span> Offer
+        </h2>
+        <p className="mt-4 lg:max-w-lg info-text">
+          Embark on a shopping journey that redefines your experience with
+          unbeatable deals. From premier selection to incredible savings, we
+          offer unparalleled value that sets us apart.
+        </p>
+        <p className="mt-6 lg:max-w-lg info-text">
+          Navigate a relam of possibilities designed to fulfill your unique
+          desires, surpassing the loftiest expectations. Your journey with us is
+          nothing short of exceptional.
+        </p>
+        <div className="mt-11 flex flex-wrap gap-4">
+          <Button label="Shop now" iconURL={arrowRight}/>
+          <Button label='Learn more' backgroundColor='bg-white' borderColor='border-slate-gray' textColor='text-slate-gray' />
+        </div>
       </div>
     </section>
   );
