3facd12cde20d2a6649933019dba1f6fb5cd2af3
veronosmani <veron.osmani@gmail.com>
2024-06-21 10:31:00 +0200

    modified:   src/components/Button.jsx 	modified:   src/sections/SuperQuality.jsx

--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -1,37 +1,38 @@
-import { shoe8 } from'../assets/images';
+import { shoe8 } from "../assets/images";
 import { Button } from "../components/Button";
 
 const SuperQuality = () => {
   return (
     <section
-      id="about-us"
-      className="flex justify-between items-center max-lg:flex-col gap-10 w-full max-container"
+      id='about-us'
+      className='flex justify-between items-center max-lg:flex-col gap-10 w-full max-container'
     >
-      <div className="flex flex-1 flex-col">
-        <h2 className="font-palanquin text-4xl capitalize font-bold lg:max-w-lg">
+      <div className='flex flex-1 flex-col'>
+        <h2 className='font-palanquin capitalize text-4xl lg:max-w-lg font-bold'>
           We Provide You
-          <span className="text-coral-red"> Super </span>
-          <span className="text-coral-red">Quality </span>Shoes
+          <span className='text-coral-red'> Super </span>
+          <span className='text-coral-red'>Quality </span> Shoes
         </h2>
-        <p className="mt-4 lg:max-w-lg info-text">
+        <p className='mt-4 lg:max-w-lg info-text'>
           Ensuring premium comfort and style, our meticulously crafted footwear
           is designed to elevate your experience, providing you with unmatched
-          quality, innovation and a touch of elegance.
+          quality, innovation, and a touch of elegance.
         </p>
-        <p className="mt-6 lg:max-w-lg info-text">
+        <p className='mt-6 lg:max-w-lg info-text'>
           Our dedication to detail and excellence ensures your satisfaction
         </p>
-        <div className="mt-11">
-          <Button label="View Details" />
+        <div className='mt-11'>
+          <Button label='View details' />
         </div>
       </div>
-      <div className="flex-1 flex justify-center items-center">
+
+      <div className='flex-1 flex justify-center items-center'>
         <img
           src={shoe8}
-          alt="shoe8"
+          alt='product detail'
           width={570}
           height={522}
-          className="object-contain"
+          className='object-contain'
         />
       </div>
     </section>
